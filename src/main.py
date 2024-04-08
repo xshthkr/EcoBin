9f9683bdaabde5fb393e48a9fbee21cd5716539c
Shashank Thakur <shashankt2004@gmail.com>
2024-04-08 21:48:57 +0530

    threaded main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from image_capture import ImageCapture
-from servo_controller import ServoController
+from servo_controller_threaded import ServoController
 from classification_2 import ImageClassifier
 
 import os
@@ -18,21 +18,30 @@ result = model.classify_image(path_to_image)
 
 servo1 = ServoController(11)
 
-def func1():
+def servo1_spin_clockwise():
+    servo1 = ServoController(11)
     servo1.move_servo(0)
-    servo2.move_servo(180)
-
-def func2():
     servo1.move_servo(180)
-    servo2.move_servo(0)
+    del servo1
 
-def func3():
+def servo1_spin_counterclockwise():
+    servo1 = ServoController(11)
     servo1.move_servo(180)
-    servo2.move_servo(0)
-
-def func4():
     servo1.move_servo(0)
+    del servo1
+
+def servo2_spin_clockwise():
+    servo2 = ServoController(37)
+    servo2.move_servo(0)
     servo2.move_servo(180)
+    del servo2
+
+def servo2_spin_counterclockwise():
+    servo2 = ServoController(37)
+    servo2.move_servo(180)
+    servo2.move_servo(0)
+    del servo2
+
 
 if result == "O":
     print(f"[ECOBIN] >> Predicted Label: {result}")
@@ -40,8 +49,8 @@ if result == "O":
 
 elif result == "R":
     print(f"[ECOBIN] >> Predicted Label: {result}")
-    t1 = threading.Thread(target=func1)
-    t2 = threading.Thread(target=func2)
+    t1 = threading.Thread(target=servo1_spin_clockwise)
+    t2 = threading.Thread(target=servo2_spin_counterclockwise)
     t1.start()
     t2.start()
     t1.join()
@@ -49,8 +58,8 @@ elif result == "R":
 
 else:
     print("[ECOBIN] >> Couldnt classify image.")    
-    t1 = threading.Thread(target=func3)
-    t2 = threading.Thread(target=func4)
+    t1 = threading.Thread(target=servo1_spin_counterclockwise)
+    t2 = threading.Thread(target=servo2_spin_clockwise)
     t1.start()
     t2.start()
     t1.join()

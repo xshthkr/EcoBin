d065fa47374b13d49be017eb70cc15434d4b8fea
Shashank Thakur <shashankt2004@gmail.com>
2024-04-09 13:57:56 +0530

    fix

--- src/main.py
+++ src/main.py
@@ -29,8 +29,6 @@ servo2 = ServoController(37, 2)
 def servo1_spin_clockwise():
     servo1.move_servo(0)
     servo1.move_servo(90)
-
-
 def servo2_spin_counterclockwise():
     servo2.move_servo(90)
     servo2.move_servo(0)
@@ -39,8 +37,6 @@ def servo2_spin_counterclockwise():
 def servo1_spin_counterclockwise():
     servo1.move_servo(90)
     servo1.move_servo(0)
-
-
 def servo2_spin_clockwise():
     servo2.move_servo(0)
     servo2.move_servo(90)

e8ba04d953358a18143559efe536e6dc7e85a863
Shashank Thakur <shashankt2004@gmail.com>
2024-04-09 14:20:09 +0530

    fix

--- src/servo_controller_threaded.py
+++ src/servo_controller_threaded.py
@@ -55,8 +55,8 @@ def servo2_spin_counterclockwise():
 
 # PAIR 2
 def servo1_spin_counterclockwise():
-    servo1.move_servo(90)
     servo1.move_servo(0)
+    servo1.move_servo(90)
 def servo2_spin_clockwise():
     servo2.move_servo(0)
     servo2.move_servo(90)
